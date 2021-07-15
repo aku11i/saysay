221bed47bf62dc60c2b4f78d49f0ac875c2ad2ed
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-07-15 21:21:07 +0900

    Setup Electron, vite, esbuild

---
+++ src/main/index.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadFile(
+    path.join(app.getAppPath(), "dist", "renderer", "index.html")
+  );
+
+  mainWindow.webContents.openDevTools();
+}
+
+(async () => {
+  await app.whenReady();
+
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+  });
+})();
