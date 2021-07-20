ec99a2e024af479a4829bdaedc5e64795e8f6e02
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-07-20 23:48:04 +0900

    chore: Update comments

--- dev.ts
+++ dev.ts
@@ -6,25 +6,29 @@ import * as path from "path";
 import * as url from "url";
 
 const require = module.createRequire(import.meta.url);
-const ROOT = path.dirname(url.fileURLToPath(import.meta.url));
 
 const chokidar = require("chokidar") as typeof import("chokidar");
 const chalk = require("chalk") as typeof import("chalk");
 const vite = require("vite") as typeof import("vite");
 
+const ROOT = path.dirname(url.fileURLToPath(import.meta.url));
+
 (async () => {
+  // build main process
   await vite.build({
     mode: "development",
     configFile: path.join(ROOT, "vite.config.main.ts"),
     build: { watch: {}, sourcemap: "inline" },
   });
 
+  // build preload process
   await vite.build({
     mode: "development",
     configFile: path.join(ROOT, "vite.config.preload.ts"),
     build: { watch: {}, sourcemap: "inline" },
   });
 
+  // serve renderer process
   const server = await vite.createServer({
     mode: "development",
     configFile: path.join(ROOT, "vite.config.renderer.ts"),
@@ -36,6 +40,7 @@ const vite = require("vite") as typeof import("vite");
 
   await server.listen();
 
+  // wait for first build
   await new Promise((r) => setTimeout(r, 5000));
 
   let electronProcess: childProcess.ChildProcess | undefined;
@@ -58,6 +63,8 @@ const vite = require("vite") as typeof import("vite");
 
   chokidar.watch(watchDirs).on("all", (event, path) => {
     if (event !== "add" && event !== "change") return;
+
+    // restart app when main/preload files are changed
     console.log(chalk.yellow(`[${event}] ${path}`));
     restartElectron();
   });
