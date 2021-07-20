76e065cbbf4828325960153d95d70d39a068b38a
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-07-20 20:58:24 +0900

    feat: sayコマンドが見つからない場合にエラーにする

--- main/helpers/siri.ts
+++ main/helpers/siri.ts
@@ -1,13 +1,26 @@
 import { execFile } from "child_process";
 import { BrowserWindow, dialog, shell } from "electron";
+import { promises as fs } from "fs";
 import * as path from "path";
 import { promisify } from "util";
+
+import { CommandNotFoundError } from "../../common/errors/CommandNotFoundError";
 
 const execFileAsync = promisify(execFile);
 
 const SAY_COMMAND = "/usr/bin/say";
 
+const searchSayCommand = async () => {
+  try {
+    await fs.stat(SAY_COMMAND);
+  } catch (e) {
+    throw new CommandNotFoundError(`Command "${SAY_COMMAND}" is not found.`);
+  }
+};
+
 export const say = async (message: string): Promise<void> => {
+  await searchSayCommand();
+
   await execFileAsync(SAY_COMMAND, [message]);
 };
 
@@ -15,6 +28,8 @@ export const save = async (
   message: string,
   win: BrowserWindow
 ): Promise<string | void> => {
+  await searchSayCommand();
+
   const { canceled, filePath } = await dialog.showSaveDialog(win);
   if (canceled || !filePath) return;
 
